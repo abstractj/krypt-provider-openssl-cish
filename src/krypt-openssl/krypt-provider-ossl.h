6a4d43d684f8a318d4745a95015450db79fbfa92
Bruno Oliveira <bruno@abstractj.org>
2013-06-15 11:58:54 -0300

    Inclusion of C files from Krypt

---
+++ src/krypt-openssl/krypt-provider-ossl.h
@@ -0,0 +1,63 @@
+/*
+ * krypt-provider-openssl - Implementation using OpenSSL
+ *
+ * Copyright (c) 2011-2013
+ * Hiroshi Nakamura <nahi@ruby-lang.org>
+ * Martin Bosslet <martin.bosslet@gmail.com>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef _KRYPT_PROVIDER_OSSL_H_
+#define _KRYPT_PROVIDER_OSSL_H_
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+#include <openssl/opensslv.h>
+
+#if defined(_WIN32)
+#  include <openssl/e_os2.h>
+#  define OSSL_NO_CONF_API 1
+#  ifndef OPENSSL_SYS_WIN32
+#    define OPENSSL_SYS_WIN32 1
+#  endif
+#  include <winsock2.h>
+#endif
+
+#include <errno.h>
+#include <openssl/err.h>
+#include <openssl/ssl.h>
+
+extern krypt_provider krypt_provider_ossl;
+krypt_provider *krypt_ossl_provider_get(void);
+
+void krypt_ossl_init(krypt_provider *provider, void *args);
+void krypt_ossl_finalize(krypt_provider *provider);
+krypt_md *krypt_ossl_md_new_oid(krypt_provider *provider, const char *oid);
+krypt_md *krypt_ossl_md_new_name(krypt_provider *provider, const char *name);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif /* _KRYPT_PROVIDER_H_ */
---
+++ src/krypt-openssl/krypt_ossl_digest.c
@@ -0,0 +1,202 @@
+/*
+ * krypt-provider-openssl - Implementation using OpenSSL
+ *
+ * Copyright (c) 2011-2013
+ * Hiroshi Nakamura <nahi@ruby-lang.org>
+ * Martin Bosslet <martin.bosslet@gmail.com>
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#include "krypt-provider.h"
+#include "krypt-provider-ossl.h"
+
+typedef struct krypt_md_ossl_st {
+    krypt_provider *provider;
+    krypt_interface_md *methods;
+    EVP_MD_CTX *ctx;
+} krypt_md_ossl;
+
+#define int_safe_cast(dest, md)						\
+do {									\
+    if ((md)->provider != &krypt_provider_ossl) return KRYPT_ERR;	\
+    (dest) = (krypt_md_ossl *) (md);					\
+} while (0)
+
+int int_md_reset(krypt_md *md);
+int int_md_update(krypt_md *md, const void *data, size_t len);
+int int_md_final(krypt_md *md, uint8_t ** digest, size_t *len);
+int int_md_digest(krypt_md *md, const uint8_t *data, size_t len, uint8_t **digest, size_t *digest_len);
+int int_md_digest_length(krypt_md *md, size_t *len);
+int int_md_block_length(krypt_md *md, size_t *len);
+int int_md_name(krypt_md *md, const char **name);
+void int_md_mark(krypt_md *md);
+void int_md_free(krypt_md *md);
+
+krypt_interface_md krypt_interface_md_ossl = {
+    int_md_reset,
+    int_md_update,
+    int_md_final,
+    int_md_digest,
+    int_md_digest_length,
+    int_md_block_length,
+    int_md_name,
+    NULL,
+    int_md_free
+};
+
+static krypt_md_ossl *
+int_md_alloc(void)
+{
+    krypt_md_ossl *md = (krypt_md_ossl *) malloc(sizeof(krypt_md_ossl));
+    memset(md, 0, sizeof(krypt_md_ossl));
+    md->provider = &krypt_provider_ossl;
+    md->methods = &krypt_interface_md_ossl;
+    return md;
+}
+
+#define return_krypt_ossl_md()					\
+do {								\
+    if (!md) return NULL;					\
+    if (!(ctx = EVP_MD_CTX_create())) return NULL;		\
+    if (!(ret = int_md_alloc())) return NULL;			\
+    if (EVP_DigestInit_ex(ctx, md, NULL) != 1) return NULL;	\
+    ret->ctx = ctx;						\
+    return (krypt_md *)ret;					\
+} while (0)
+
+krypt_md *
+krypt_ossl_md_new_oid(krypt_provider *provider, const char *oid)
+{
+    EVP_MD_CTX *ctx;
+    const EVP_MD *md;
+    krypt_md_ossl *ret;
+    ASN1_OBJECT *ossl_oid;
+
+    if (provider != &krypt_provider_ossl) return NULL;
+
+    ossl_oid = OBJ_txt2obj(oid, 0);
+    md = EVP_get_digestbyobj(ossl_oid);
+    ASN1_OBJECT_free(ossl_oid);
+    return_krypt_ossl_md();
+}
+
+krypt_md *
+krypt_ossl_md_new_name(krypt_provider *provider, const char *name)
+{
+    EVP_MD_CTX *ctx;
+    const EVP_MD *md;
+    krypt_md_ossl *ret;
+
+    if (provider != &krypt_provider_ossl) return NULL;
+
+    md = EVP_get_digestbyname(name);
+    return_krypt_ossl_md();
+}
+
+int
+int_md_reset(krypt_md *ext)
+{
+    krypt_md_ossl *md;
+
+    int_safe_cast(md, ext);
+    if (EVP_DigestInit_ex(md->ctx, EVP_MD_CTX_md(md->ctx), NULL) != 1)
+	return KRYPT_ERR;
+    return KRYPT_OK;
+}
+
+int
+int_md_update(krypt_md *ext, const void *data, size_t len)
+{
+    krypt_md_ossl *md;
+
+    int_safe_cast(md, ext);
+    EVP_DigestUpdate(md->ctx, data, len);
+    return KRYPT_OK;
+}
+
+int
+int_md_final(krypt_md *ext, uint8_t ** digest, size_t *len)
+{
+    krypt_md_ossl *md;
+    uint8_t *ret;
+    size_t ret_len;
+
+    int_safe_cast(md, ext);
+    ret_len = EVP_MD_CTX_size(md->ctx);
+    ret = (uint8_t *) malloc(ret_len);
+    EVP_DigestFinal_ex(md->ctx, (unsigned char *)ret, NULL);
+
+    *digest = ret;
+    *len = ret_len;
+    return KRYPT_OK;
+}
+
+int
+int_md_digest(krypt_md *ext, const uint8_t *data, size_t len, uint8_t **digest, size_t *digest_len)
+{
+    if (!int_md_update(ext, data, len)) return KRYPT_ERR;
+    return int_md_final(ext, digest, digest_len);
+}
+
+int
+int_md_digest_length(krypt_md *ext, size_t *len)
+{
+    krypt_md_ossl *md;
+
+    int_safe_cast(md, ext);
+    *len = EVP_MD_CTX_size(md->ctx);
+    return KRYPT_OK;
+}
+
+int
+int_md_block_length(krypt_md *ext, size_t *len)
+{
+    krypt_md_ossl *md;
+
+    int_safe_cast(md, ext);
+    *len = EVP_MD_CTX_block_size(md->ctx);
+    return KRYPT_OK;
+}
+
+int
+int_md_name(krypt_md *ext, const char **name)
+{
+    krypt_md_ossl *md;
+
+    int_safe_cast(md, ext);
+    *name = EVP_MD_name(EVP_MD_CTX_md(md->ctx));
+    return KRYPT_OK;
+}
+
+void
+int_md_free(krypt_md *ext)
+{
+    krypt_md_ossl *md;
+
+    if (!ext) return;
+    if ((ext)->provider != &krypt_provider_ossl) return;
+    md = (krypt_md_ossl *) ext;
+    if (md->ctx)
+	EVP_MD_CTX_destroy(md->ctx);
+    free(md);
+}
+
